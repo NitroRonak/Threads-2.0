9e7cce347e8a4a452085ca5cc661c46c483d5f2f
NitroRonak <ronaks0988@gmail.com>
2024-02-03 22:21:02 +0530

    Basic Setup for Nextjs and Clerk Done

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,29 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Mulish } from "next/font/google";
+import { dark } from "@clerk/themes";
+import "../globals.css"
+export const metadata = {
+  title: "Threads",
+  description: "A Next.js Meta Threads Application",
+};
+
+const mulish = Mulish({ subsets: ["latin"] });
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en">
+        <body className={`${mulish.className} bg-dark-1`}>
+            {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
