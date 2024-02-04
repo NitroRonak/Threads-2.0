f62bda7cb942697f38bb6e27c6c14821835ef3c7
NitroRonak <ronaks0988@gmail.com>
2024-02-04 23:52:45 +0530

    Bottombar UI Done.

--- Components/shared/Bottombar.tsx
+++ Components/shared/Bottombar.tsx
@@ -1,10 +1,36 @@
+"use client";
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 
 const Bottombar = () => {
+  const router = useRouter();
+  const pathname = usePathname();
   return (
-    <div>
-      Bottombar
-    </div>
-  )
-}
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link) => {
+          const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
 
-export default Bottombar
+          return (
+            <Link
+              key={link.label}
+              href={link.route}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+              <span className="text-subtle-medium text-light-1 max-sm:hidden">
+
+                {link.label.split(/\s+/)[0]}
+
+              </span>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Bottombar;
