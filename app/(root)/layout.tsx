452001fe882b12dcdd39cfb9fe92575045ec0ad2
NitroRonak <ronaks0988@gmail.com>
2024-02-17 17:56:26 +0530

    Removed some styling from globals.css file

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,18 @@
 import type { Metadata } from "next";
 import { Mulish } from "next/font/google";
-import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Topbar from "@/Components/shared/Topbar";
 import LeftSidebar from "@/Components/shared/LeftSidebar";
 import RightSidebar from "@/Components/shared/RightSidebar";
 import Bottombar from "@/Components/shared/Bottombar";
 import { dark } from "@clerk/themes";
+import "../globals.css"
 const mulish = Mulish({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Threads",
   description: "A Next.js Meta Threads Application",
 };
-
 
 export default function RootLayout({
   children,
@@ -28,19 +27,17 @@ export default function RootLayout({
     >
       <html lang="en">
         <body className={mulish.className}>
-          <Topbar/>
+          <Topbar />
           <main className="flex flex-row">
-            <LeftSidebar/>
+            <LeftSidebar />
 
             <section className="flex min-h-screen flex-1 flex-col items-center bg-dark-1 px-6 pb-10 pt-28 max-md:pb-32 sm:px-10">
-              <div className="w-full max-w-4xl">
-                {children}
-              </div>
+              <div className="w-full max-w-4xl">{children}</div>
             </section>
 
-            <RightSidebar/>
+            <RightSidebar />
           </main>
-          <Bottombar/>
+          <Bottombar />
         </body>
       </html>
     </ClerkProvider>
